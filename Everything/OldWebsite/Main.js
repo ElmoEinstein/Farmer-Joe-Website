b3f26c701c510d27833640ccc58b060468360918
ElmoEinstein <lowekasper@gmail.com>
2024-07-10 01:49:49 +1000

    newnewnew

--- Everything/OldWebsite/Main.js
+++
@@ -1,7 +0,0 @@
-const parallax = document.getElementById("parallax");
-
-window.addEventListener("scroll", function (){
-	let offset = window.pageYOffset;
-	console.log(parallax.style);
-	parallax.style.backgroundPositionY = offset * .5 + "px" ;
-})
